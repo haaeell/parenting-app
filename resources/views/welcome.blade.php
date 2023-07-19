14f2ac8923323860c4df0514b18c3f3043520369
yolaaamr <yolamutiara2@gmail.com>
2023-07-19 18:15:26 +0700

    coba push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.landingpage')
 
 @section('content')
-    <h1>landingpage</h1>
+    <div class="container p-4">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+            <h4>Welcome to Parenting</h4>
+            <h1>Parenting Path: Nurturing Futures, One Step at a Time</h1>
+            <h4>Creating Stronger Bonds, Raising Exceptional Children</h4>                
+            <a href="#" class="btn btn-primary">About us</a>
+            </div>
+            <div class="col-md-6">
+                <img src="path/to/your/image.jpg" alt="Hero Image" class="img-fluid rounded">
+            </div>
+        </div>
+    </div>
 @endsection
