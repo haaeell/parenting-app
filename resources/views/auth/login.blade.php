b60f4967f5624e77c233729bad893ccacfbd3087
Ulannnnnnn <triwulandari.tw71@gmail.com>
2023-07-22 21:19:09 +0700

    komentar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.landingpage')
+
+@section('css')
+<style>
+    .card {
+        border: none;
+        border-bottom: 1px solid #ccc;
+        border-radius: 32px;
+        padding: 5px 10px;
+        margin-bottom: 10px;
+    }
+    .form-group input {
+        border: none;
+        border-bottom: 1px solid #ccc;
+        padding: 5px 0;
+    }
+    .forgot-password {
+        text-align: right;
+        font-size: 12px;
+    }
+    .btn-custom {
+        background-color: #659384;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 50px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+    .btn-custom:hover {
+        background-color: #C2D2C5;
+
+</style>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<div style="background-color: #C2D2C5">
+    <div class="container">
+        <div class="row d-flex justify-content-center align-items-center gap-5 vh-100">
+            <div class="col-md-4">
+                <div class="card p-5">
+                    <div class="card-body">
+                        <h5 class="fw-bold text-center mb-5" >Sign In</h5>
+                        <form>
+                            <div class="form-group mb-5">
+                                <input type="email" class="form-control" id="email" placeholder="Enter email">
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <div class="form-group mb-5">
+                                <input type="password" class="form-control" id="password" placeholder="Password">
+                                <div class="forgot-password">
+                                    <span>Forgot password</span>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="fw-bold btn-custom mx-auto d-block mb-3">Sign In</button>
+                            <p style="font-size: 12px;" class="text-center">Don’t have an account? Create account</p>
+                        </form>
+                    </div>
                 </div>
+            </div>
+            <div class="col-md-6">
+                <img src="{{asset('image/illustrationFamily.png')}}" alt="Image" class="img-fluid">
             </div>
         </div>
     </div>
 </div>
+
 @endsection
